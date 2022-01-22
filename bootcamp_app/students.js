ed996476cc6abce1f6b91ba366677f6398c4728f
notlucaslee <lee.lucas.hyunguk@gmail.com>
2022-01-22 09:35:19 +0000

    change queries to paramerized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,5 +1,13 @@
-const cohortQuery = process.argv[2];
-const queryLimit = process.argv[3];
+const cohortName = process.argv[2];
+const limit = process.argv[3];
+const values = [`%${cohortName}%`, limit];
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
 
 const { Pool } = require('pg');
 
@@ -10,16 +18,10 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT students.id, students.name, cohorts.name as cohort_name
-FROM students
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '${cohortQuery}%'
-LIMIT ${queryLimit};
-`)
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
 })
 .catch(err => console.error('query error', err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,15 @@
 const cohort = process.argv[2];
 const { Pool } = require('pg');
+const value = [cohort];
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort 
+FROM teachers 
+JOIN assistance_requests ON teacher_id = teachers.id 
+JOIN students ON student_id = students.id 
+JOIN cohorts ON cohort_id = cohorts.id 
+WHERE cohorts.name = $1
+ORDER BY teacher;
+`;
 
 const pool = new Pool({
   user: 'vagrant',
@@ -8,15 +18,7 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort 
-FROM teachers 
-JOIN assistance_requests ON teacher_id = teachers.id 
-JOIN students ON student_id = students.id 
-JOIN cohorts ON cohort_id = cohorts.id 
-WHERE cohorts.name = '${cohort}'
-ORDER BY teacher;
-`)
+pool.query(queryString, value)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${cohort}: ${row.teacher}`);
